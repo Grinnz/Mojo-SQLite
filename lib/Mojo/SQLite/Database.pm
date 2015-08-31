81a4a9a44c463a086aabb1b7d3485dea76e912b0
Dan Book <grinnz@grinnz.com>
2015-08-31 18:26:59 -0400

    Bump version

--- lib/Mojo/SQLite/Database.pm
+++ lib/Mojo/SQLite/Database.pm
@@ -8,7 +8,7 @@ use Mojo::SQLite::Results;
 use Mojo::SQLite::Transaction;
 use Scalar::Util 'weaken';
 
-our $VERSION = '0.012';
+our $VERSION = '0.013';
 
 our @CARP_NOT = qw(Mojo::SQLite::Migrations);
 
--- lib/Mojo/SQLite/Transaction.pm
+++ lib/Mojo/SQLite/Transaction.pm
@@ -1,7 +1,7 @@
 package Mojo::SQLite::Transaction;
 use Mojo::Base -base;
 
-our $VERSION = '0.012';
+our $VERSION = '0.013';
 
 has 'db';
 
