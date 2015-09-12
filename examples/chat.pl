4424e35d7d819706cc380007a43e4580630e7867
Dan Book <grinnz@grinnz.com>
2015-09-11 20:07:36 -0400

    add chat example

---
+++ examples/chat.pl
@@ -0,0 +1,33 @@
+use Mojolicious::Lite;
+use Mojo::SQLite;
+
+helper sqlite => sub { state $sql = Mojo::SQLite->new };
+
+get '/' => 'chat';
+
+websocket '/channel' => sub {
+  my $c = shift;
+
+  $c->inactivity_timeout(3600);
+
+  # Forward messages from the browser to PostgreSQL
+  $c->on(message => sub { shift->sqlite->pubsub->notify(mojochat => shift) });
+
+  # Forward messages from PostgreSQL to the browser
+  my $cb = $c->sqlite->pubsub->listen(mojochat => sub { $c->send(pop) });
+  $c->on(finish => sub { shift->sqlite->pubsub->unlisten(mojochat => $cb) });
+};
+
+app->start;
+__DATA__
+
+@@ chat.html.ep
+<form onsubmit="sendChat(this.children[0]); return false"><input></form>
+<div id="log"></div>
+<script>
+  var ws  = new WebSocket('<%= url_for('channel')->to_abs %>');
+  ws.onmessage = function (e) {
+    document.getElementById('log').innerHTML += '<p>' + e.data + '</p>';
+  };
+  function sendChat(input) { ws.send(input.value); input.value = '' }
+</script>
